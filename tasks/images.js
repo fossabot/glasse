fc8eb98d3d0a4d6ab29ff0e4ab9d0ca9205247ec
Gustav Pursche <gustav.pursche@aboutsource.net>
2016-05-11 16:04:16 +0200

    Remove `svg-sprite` subtask from image task

--- tasks/images.js
+++ tasks/images.js
@@ -5,15 +5,10 @@ const distDir = require('../lib/gulp/dist-dir');
 const plugins = require('../lib/gulp/plugins').read()
 const tasks = require('../lib/gulp/tasks');
 
-module.exports = {
-  dep: [
-    'svg-sprite',
-  ],
-  fn: (gulp, projectArguments) => {
-    let config = configuration.merge(configuration.default(), projectArguments.config);
+module.exports = (gulp, projectArguments) => {
+  let config = configuration.merge(configuration.default(), projectArguments.config);
 
-    return gulp.src(config.images.src)
-      .pipe(tasks.images(config.images || {}, plugins, config))
-      .pipe(gulp.dest(distDir(config, 'images')));
-  },
+  return gulp.src(config.images.src)
+    .pipe(tasks.images(config.images || {}, plugins, config))
+    .pipe(gulp.dest(distDir(config, 'images')));
 };
