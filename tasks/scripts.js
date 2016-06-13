41d75bb42707b385f1dd3e49dae5efa046001ee9
Gustav Pursche <gustav.pursche@aboutsource.net>
2016-06-13 11:16:32 +0200

    Error logging for styles & scripts

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -3,12 +3,14 @@
 const configuration = require('../lib/gulp/configuration');
 const distDir = require('../lib/gulp/dist-dir');
 const plugins = require('../lib/gulp/plugins').read();
+const plumber = require('gulp-plumber');
 const tasks = require('../lib/gulp/tasks');
 
 module.exports = (gulp, projectArguments) => {
   let config = configuration.merge(configuration.default(), projectArguments.config);
 
   return gulp.src(config['scripts'].src)
+    .pipe(plumber())
     .pipe(tasks['scripts'](config['scripts'].settings, plugins, config))
     .pipe(plugins.flatten())
     .pipe(gulp.dest(distDir(config, 'scripts')));
--- tasks/styles.js
+++ tasks/styles.js
@@ -3,12 +3,14 @@
 const configuration = require('../lib/gulp/configuration');
 const distDir = require('../lib/gulp/dist-dir');
 const plugins = require('../lib/gulp/plugins').read()
+const plumber = require('gulp-plumber');
 const tasks = require('../lib/gulp/tasks');
 
 module.exports = (gulp, projectArguments) => {
   let config = configuration.merge(configuration.default(), projectArguments.config);
 
   return gulp.src(config['styles'].src)
+    .pipe(plumber())
     .pipe(tasks['styles'](config['styles'].settings, plugins, config))
     .pipe(plugins.flatten())
     .pipe(gulp.dest(distDir(config, 'styles')));
