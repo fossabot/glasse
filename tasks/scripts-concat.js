349205c3739ef5c25b49deb4de9bd0c7471d3d00
Gustav Pursche <gustav.pursche@aboutsource.net>
2016-06-29 16:02:51 +0200

    Fix concat streams

--- tasks/scripts-concat.js
+++ tasks/scripts-concat.js
@@ -3,35 +3,38 @@
 const _ = require('lodash');
 const concatIntoFile = require('../lib/gulp/concat-into-file.js');
 const configuration = require('../lib/gulp/configuration');
-const merge = require('merge-stream');
+const eventStream = require('event-stream');
+const gutil = require('gulp-util');
 const plugins = require('../lib/gulp/plugins').read();
 const tasks = require('../lib/gulp/tasks');
 
 module.exports = (gulp, projectArguments) => {
-  let config = configuration.merge(configuration.default(), projectArguments.config);
+  const config = configuration.merge(configuration.default(), projectArguments.config);
+  const toConcat = config['scripts'].concat;
+  const buildFile = function(file) {
+    gutil.log('Concat File:', file.name);
 
-  let merged = merge();
-  let toConcat = config['scripts'].concat;
-
-  let buildFile = function(file) {
     let src = config.scripts.src;
-    let filter = file.filter || [];
+    let filter = file.filter || function() {
+      return true;
+    };
     let union = file.union || undefined;
 
     src = _.union(src, union);
 
-    let fileStream = gulp.src(src)
+    return gulp.src(src)
       .pipe(plugins.filter(filter))
       .pipe(tasks['scripts'](file.settings, plugins, config))
       .pipe(concatIntoFile(file, plugins, config));
-
-    merged.add(fileStream);
   };
+
+  const concatStreams = toConcat.map((file) => {
+    return buildFile(file);
+  });
 
   if(!toConcat.length) {
     return merged;
   }
 
-  toConcat.forEach(buildFile);
-  return merged;
+  return eventStream.merge(concatStreams);
 };
--- tasks/styles-concat.js
+++ tasks/styles-concat.js
@@ -3,35 +3,38 @@
 const _ = require('lodash');
 const concatIntoFile = require('../lib/gulp/concat-into-file.js');
 const configuration = require('../lib/gulp/configuration');
-const merge = require('merge-stream');
+const eventStream = require('event-stream');
+const gutil = require('gulp-util');
 const plugins = require('../lib/gulp/plugins').read();
 const tasks = require('../lib/gulp/tasks');
 
 module.exports = (gulp, projectArguments) => {
-  let config = configuration.merge(configuration.default(), projectArguments.config);
+  const config = configuration.merge(configuration.default(), projectArguments.config);
+  const toConcat = config['styles'].concat;
+  const buildFile = function(file) {
+    gutil.log('Concat File:', file.name);
 
-  let merged = merge();
-  let toConcat = config['styles'].concat;
-
-  let buildFile = function(file) {
     let src = config.styles.src;
-    let filter = file.filter || [];
-    let union = file.union || undefined;
+    const filter = file.filter || function() {
+      return true;
+    };
+    const union = file.union || undefined;
 
     src = _.union(src, union);
 
-    let fileStream = gulp.src(src)
+    return gulp.src(src)
       .pipe(plugins.filter(filter))
       .pipe(tasks['styles'](file.settings, plugins, config))
       .pipe(concatIntoFile(file, plugins, config));
-
-    merged.add(fileStream);
   };
+
+  const concatStreams = toConcat.map((file) => {
+    return buildFile(file);
+  });
 
   if(!toConcat.length) {
     return merged;
   }
 
-  toConcat.forEach(buildFile);
-  return merged;
+  return eventStream.merge(concatStreams);
 };
