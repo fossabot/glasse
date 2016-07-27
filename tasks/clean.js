06802c74805ba0bd5acd7ecae63cbf8ed5edfc21
Gustav Pursche <gustav.pursche@aboutsource.net>
2016-07-27 14:21:15 +0200

    Replace `gulp-clean` with `del`, Empty `dist/` instead of removing

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const configuration = require('../lib/gulp/configuration');
+const del = require('del');
 const path = require('path');
 const plugins = require('../lib/gulp/plugins').read()
 const tasks = require('../lib/gulp/tasks');
 
 module.exports = (gulp, projectArguments) => {
   let config = configuration.merge(configuration.default(), projectArguments.config);
-  let options = {
-    read: false,
-  };
 
-  return gulp.src(config.dist_dir, options)
-    .pipe(plugins.clean());
+  return del([
+    path.join(config.dist_dir, '**/*'),
+  ])
 };
